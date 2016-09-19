d82374bb3cf992f100be9dca28fd06b45669de0f
Danilo <dznery@gmail.com>
2016-09-19 18:00:53 -0300

    Include step-by-step detailed solution
    Explained solution provided in the ihaskell-notebook file (.ipynb).
    Corresponding haskell program also rewritten to match the above.

--- haskell/solution.hs
+++ haskell/solution.hs
@@ -1,18 +1,29 @@
+-- weave insert first into second.
+--
+--       i: [11,12], [0,1,2,3]
+--       o: [0,11,1,12,2,11,3]
+--
 weave :: [a] -> [a] -> [a]
 weave [] b = b
 weave a [] = a
-weave a b = weave' (cycle a) b
+weave a b  = init $ concat $ zipWith (\x y -> [y, x]) (cycle a) b
 
-weave' :: [a] -> [a] -> [a]
-weave' _ b@(x:[]) = b
-weave' a@(x:xs) b@(y:ys) = y:x: weave' xs ys
+-- bracket insert first into second.
+-- b with even length is obligatory.
+--
+--       i: "abc", "()"
+--       o: ["(a)","(b)","(c)"]
+--
+--       i: "+-", "234567"
+--       o: ["2+3", "4-5", "6+7"]
+--
+bracket :: [a] -> [a] -> [a]
+bracket [] _ = []
+bracket _ [] = []
+bracket a b  = if length a >= length b `div` 2
+                  then concat $ zipWith (\x (ya, yb) -> [ya, x, yb]) a $ cycle (zipPairs b)
+                  else concat $ zipWith (\x (ya, yb) -> [ya, x, yb]) (cycle a) $ zipPairs b
 
-bracket :: [a] -> [a] -> [[a]]
-bracket a b
-  | (length a) >= (length b) `quot` 2 = bracket' a (cycle b)
-  | (length a) < (length b) `quot` 2 = bracket' (cycle a) b
-
-bracket' :: [a] -> [a] -> [[a]]
-bracket' [] _ = []
-bracket' _ [] = []
-bracket' (x:xs) (n:m:ys) = [n:x:m:[]] ++ bracket' xs ys
+zipPairs :: [a] -> [(a, a)]
+zipPairs (xa:xb:[]) = (xa, xb):[]
+zipPairs (xa:xb:xs) = (xa, xb):(zipPairs xs)
